c935b8a6eece6e5960f916c0fb1f370ab46c4893
Tung Truong <tungtruongthe@gmail.com>
2022-12-29 23:59:14 +0700

    [ADD] add API service + Movie Struct

--- MovieAppLite/Service/Models/Movie.swift
+++ MovieAppLite/Service/Models/Movie.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct MovieList: Codable{
+    let page: Int?
+    let results: [MovieListResult]?
+    let totalPages: Int?
+    let totalResults: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+struct MovieListResult: Codable{
+    let adult: Bool?
+    let backdropPath: String?
+    let genreID: [Int]?
+    let id: Int?
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreID = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
