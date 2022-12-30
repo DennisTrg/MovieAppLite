f9c7ea42187948cf45b9379e9d3355a68795ae07
Tung Truong <tungtruongthe@gmail.com>
2022-12-30 17:20:18 +0700

    [CREATE] pass data to collection view using Rx swift

--- MovieAppLite/Presentation/HomeView/HomeVC.swift
+++ MovieAppLite/Presentation/HomeView/HomeVC.swift
@@ -7,19 +7,20 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
+
 class HomeVC: UIViewController {
+    var result: MovieListResult?
+    let disposeBag = DisposeBag()
+    let viewModel = HomeViewModel()
     
-    let viewModel = HomeVcViewModel()
-     lazy var collectionView: UICollectionView = {
+     var collectionView: UICollectionView = {
          let layout = UICollectionViewFlowLayout()
          layout.sectionInset = UIEdgeInsets(top: 20, left: 24, bottom: 20, right: 24)
          
-         let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
          collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
-         //collectionView.backgroundColor = .clear
-         
-         collectionView.delegate = self
-         collectionView.dataSource = self
          
         return collectionView
     }()
@@ -27,53 +28,54 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Home"
-        
+
         setupView()
-        viewModel.requestData(page: 1)
+        setupData()
     }
     
+    //MARK: Set up View
     func setupView(){
         self.view.addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
             make.edges.equalTo(self.view.safeAreaLayoutGuide)
         }
+        collectionView.rx.setDelegate(self)
     }
-
+    
+    //MARK: Pass Data
+    func setupData(){
+        viewModel.fetchMovieResult().observe(on: MainScheduler.instance)
+            .bind(to: collectionView.rx.items(cellIdentifier: HomeCollectionViewCell.identifier, cellType: HomeCollectionViewCell.self)
+        ) {(row, element, cell) in
+                cell.config(model: element)
+        }
+        .disposed(by: disposeBag)
+    }
+        
 }
 
-extension HomeVC: UICollectionViewDelegateFlowLayout{
+extension HomeVC: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.size.width * 0.41, height: view.frame.size.height * 0.26)
+        return CGSize(width: view.frame.size.width * 0.41, height: view.frame.size.height * 0.34)
     }
 }
 
-extension HomeVC: UICollectionViewDataSource{
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as? HomeCollectionViewCell else {return UICollectionViewCell()}
-        cell.backgroundColor = .red
-        return cell
-    }
-}
 
-//class Colors {
-//    var gl: CAGradientLayer?
+//        collectionView.rx.modelSelected(MovieListResult.self).subscribe { movie in
 //
-//    init() {
-//        let colorTop = UIColor(red: 3 / 255.0, green: 138.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0).cgColor
-//        let colorBottom = UIColor(red: 45.0 / 255.0, green: 85.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0).cgColor
+//        } onError: { <#Error#> in
+//            <#code#>
+//        } onCompleted: {
+//            <#code#>
+//        } onDisposed: {
 //
-//        self.gl = CAGradientLayer()
-//        self.gl?.colors = [colorTop, colorBottom]
-//        self.gl?.locations = [0.0, 1.0]
-//    }
-
-//        let color = Colors()
-//        var backgroundLayer = color.gl
-//        backgroundLayer?.frame = view.frame
-//        view.layer.insertSublayer(backgroundLayer!, at: 0)
-//}
+//        }
+//        collectionView.rx.itemSelected.subscribe { <#IndexPath#> in
+//            <#code#>
+//        } onError: { <#Error#> in
+//            <#code#>
+//        } onCompleted: {
+//            <#code#>
+//        } onDisposed: {
+//            <#code#>
+//        }
