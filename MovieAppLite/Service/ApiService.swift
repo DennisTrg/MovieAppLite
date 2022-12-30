9d9118a7174c91bffab9279b53f11b98e6163881
Tung Truong <tungtruongthe@gmail.com>
2022-12-30 10:22:56 +0700

    [FIX] combining calling APi with RxSwift in one function

--- MovieAppLite/Service/ApiService.swift
+++ MovieAppLite/Service/ApiService.swift
@@ -8,6 +8,9 @@
 import Foundation
 import RxSwift
 
+protocol ApiServiceDelegate{
+    func fetchRequest(url: String) -> Observable<MovieList>
+}
 
 class ApiService{
     static func ImageUrlString(imagePath: String) -> String{
@@ -17,45 +20,35 @@ class ApiService{
     static func urlString(category: String, page: Int) -> String{
         return "https://api.themoviedb.org/3/movie/\(category)?api_key=\(APIKey)&page=\(page)"
     }
-    
-    static func fetchData(url: String, completionHandler: @escaping (Result<MovieList, Error>) -> Void){
-        guard let url = URL(string: url) else {return}
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completionHandler(.failure(error))
-            }
-            guard let data = data else {
-                let response = response as? HTTPURLResponse
-                completionHandler(.failure(NSError(domain: "no data", code: response?.statusCode ?? -1, userInfo: nil)))
-                return
-            }
-            do{
-                let movieList = try JSONDecoder().decode(MovieList.self, from: data)
-                print(movieList)
-                completionHandler(.success(movieList))
-            } catch {
-                completionHandler(.failure(error))
-            }
-        }
-        task.resume()
-    }
 }
 
-extension ApiService{
-    static func fetchDataRxSwift(url: String) -> Observable<MovieList>{
-        return Observable.create { observer  in
-            fetchData(url: url) { result in
-                switch result{
-                case .success(let result):
-                    observer.onNext(result)
-                    observer.onCompleted()
-                case .failure(let error):
+extension ApiService: ApiServiceDelegate{
+    func fetchRequest(url: String) -> Observable<MovieList>{
+        return Observable.create { observer -> Disposable in
+            //retrieve data from URL
+            let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
+                if let error = error {
+                    observer.onError(error)
+                }
+                guard let data = data else {
+                    let response = response as? HTTPURLResponse
+                    observer.onError(NSError(domain: "No Data", code: response?.statusCode ?? -1, userInfo: nil))
+                    return
+                }
+            
+                do {
+                    //decode data
+                    let movieList = try JSONDecoder().decode(MovieList.self, from: data)
+                    observer.onNext(movieList)
+                } catch{
                     observer.onError(error)
                 }
             }
-            return Disposables.create {}
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
         }
     }
 }
-
-
